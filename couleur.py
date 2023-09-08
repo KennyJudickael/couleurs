2a8da7e8e6a75df9a9c7df93de4e65c33a471e1f
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2023-09-08 17:05:06 +0300

    Ajout de networkx pour randomiser l'affichage + demande de saisie de sommets et couleurs

--- couleur.py
+++ couleur.py
@@ -1,15 +1,26 @@
 import matplotlib.pyplot as plt
 import random
+import networkx as nx  # Pour la génération du graphe aléatoire
 
-def coloration_graphique(graph):
+
+def generer_graphe_aleatoire(nombre_de_sommets, probabilite_arrete):
+    G = nx.erdos_renyi_graph(nombre_de_sommets, probabilite_arrete)
+    return G
+
+
+def coloration_graphique(nombre_de_sommets, nombre_de_couleurs):
+    graph = generer_graphe_aleatoire(
+        nombre_de_sommets, 0.3
+    )  # Modifier la probabilité d'arête selon vos besoins
+
     def est_valide(sommet, couleur, coloration):
-        for voisin in graph[sommet]:
+        for voisin in graph.neighbors(sommet):
             if coloration[voisin] == couleur:
                 return False
         return True
 
     def coloration_util(sommet, coloration, nombre_de_couleurs):
-        if sommet == len(graph):
+        if sommet == nombre_de_sommets:
             return True
 
         for couleur in range(1, nombre_de_couleurs + 1):
@@ -19,9 +30,7 @@ def coloration_graphique(graph):
                     return True
                 coloration[sommet] = 0
 
-    nombre_de_sommets = len(graph)
     coloration = [0] * nombre_de_sommets
-    nombre_de_couleurs = 3  # Vous pouvez ajuster le nombre de couleurs ici
 
     if coloration_util(0, coloration, nombre_de_couleurs):
         print("Solution trouvée :")
@@ -29,17 +38,31 @@ def coloration_graphique(graph):
             print(f"Sommet {sommet + 1} : Couleur {couleur}")
 
         # Création du graphique
-        couleurs = [random.choice(['r', 'g', 'b', 'y', 'm', 'c']) for _ in range(nombre_de_couleurs)]
-        positions = [(random.random(), random.random()) for _ in range(nombre_de_sommets)]
+        couleurs = [
+            random.choice(["r", "g", "b", "y", "m", "c"])
+            for _ in range(nombre_de_couleurs)
+        ]
+        positions = nx.spring_layout(
+            graph
+        )  # Utilisez un algorithme de placement pour positionner les nœuds
 
         plt.figure(figsize=(8, 6))
-        for sommet, position, couleur in zip(range(nombre_de_sommets), positions, coloration):
-            plt.scatter(position[0], position[1], s=100, c=couleurs[couleur - 1], label=f"Sommet {sommet + 1}")
-        
+        for sommet, couleur in enumerate(coloration):
+            plt.scatter(
+                positions[sommet][0],
+                positions[sommet][1],
+                s=100,
+                c=couleurs[couleur - 1],
+                label=f"Sommet {sommet + 1}",
+            )
+
         # Ajout des lignes de liaison entre les sommets reliés
-        for sommet, voisins in enumerate(graph):
-            for voisin in voisins:
-                plt.plot([positions[sommet][0], positions[voisin][0]], [positions[sommet][1], positions[voisin][1]], color='k')
+        for edge in graph.edges():
+            plt.plot(
+                [positions[edge[0]][0], positions[edge[1]][0]],
+                [positions[edge[0]][1], positions[edge[1]][1]],
+                color="k",
+            )
 
         plt.legend()
         plt.title("Coloration du graphe")
@@ -47,13 +70,9 @@ def coloration_graphique(graph):
     else:
         print("Aucune solution trouvée.")
 
-# Exemple de graphe représenté par une liste d'adjacence
-graph = [
-    [1, 2, 4],  # Sommet 0 est relié à 1, 2 et 4
-    [0, 2, 3],  # Sommet 1 est relié à 0, 2 et 3
-    [0, 1, 4],  # Sommet 2 est relié à 0, 1 et 4
-    [1],        # Sommet 3 est relié à 1
-    [0, 2]      # Sommet 4 est relié à 0 et 2
-]
 
-coloration_graphique(graph)
+# Demander à l'utilisateur de saisir le nombre de sommets et le nombre de couleurs
+nombre_de_sommets = int(input("Entrez le nombre de sommets : "))
+nombre_de_couleurs = int(input("Entrez le nombre de couleurs : "))
+
+coloration_graphique(nombre_de_sommets, nombre_de_couleurs)
